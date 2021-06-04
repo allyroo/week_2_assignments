604b6ccf32521e686bcf8623c829d90c0c06fc79
Allyse Arucan <81602578+allyroo@users.noreply.github.com>
2021-06-03 23:16:01 -0700

    Add files via upload

---
+++ day3Mod.py
@@ -0,0 +1,9 @@
+from math import floor, pi
+
+def house(length,width):
+    area = length*width
+    print(f"The area of the house is {area} square feet.")
+
+def circumference(radius):
+    c = 2 * floor(pi) * radius
+    print(f"The circumference of the circle is approximately {c}.")
